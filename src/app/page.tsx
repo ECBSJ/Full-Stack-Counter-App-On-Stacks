7d199ac9986884decac8b75439ec666fd0657e4a
ecbsj <crypto67sauce@gmail.com>
2024-11-20 09:23:23 -0500

    devnet-ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 "use client"
 
-import { AppConfig, showConnect, UserSession } from "@stacks/connect"
-import Image from "next/image"
+import { createClient } from "@stacks/blockchain-api-client"
+import { AppConfig, openContractCall, showConnect, UserSession } from "@stacks/connect"
+import {
+  Cl,
+  cvToValue,
+  fetchCallReadOnlyFunction,
+  FungiblePostCondition,
+  Pc,
+  PostConditionMode,
+  StxPostCondition
+} from "@stacks/transactions"
 import { useEffect, useState } from "react"
 
 const appConfig = new AppConfig(["store_write", "publish_data"])
@@ -11,6 +20,103 @@ export default function Home() {
   const [connected, setConnected] = useState(false)
   const [globalCount, setGlobalCount] = useState(0)
   const [userCount, setUserCount] = useState(0)
+  const [countBalance, setCountBalance] = useState(0)
+  const [stxBalance, setStxBalance] = useState(0)
+
+  let userAddress = ""
+
+  if (userSession.isUserSignedIn()) {
+    userAddress = userSession.loadUserData().profile.stxAddress.testnet
+  }
+
+  const contractAddress = "ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM"
+
+  function callIncrement() {
+    openContractCall({
+      contractAddress,
+      contractName: "counter",
+      functionName: "increment",
+      functionArgs: [],
+      postConditionMode: PostConditionMode.Deny,
+      network: "devnet"
+    })
+  }
+
+  let postCondition: FungiblePostCondition | StxPostCondition
+
+  if (connected) {
+    postCondition =
+      countBalance >= 1
+        ? Pc.principal(userAddress)
+            .willSendEq(1)
+            .ft("ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM.count-token", "count-token")
+        : Pc.principal(userAddress).willSendEq(1000000).ustx()
+  }
+
+  function callDecrement() {
+    openContractCall({
+      contractAddress,
+      contractName: "counter",
+      functionName: "decrement",
+      functionArgs: [],
+      postConditions: [postCondition],
+      postConditionMode: PostConditionMode.Deny,
+      network: "devnet"
+    })
+  }
+
+  async function getTotalCount() {
+    let result = await fetchCallReadOnlyFunction({
+      contractAddress,
+      contractName: "counter",
+      functionName: "get-global-count",
+      functionArgs: [],
+      network: "devnet",
+      senderAddress: userAddress
+    })
+
+    setGlobalCount(Number(cvToValue(result)))
+  }
+
+  async function getUserCount() {
+    let result = await fetchCallReadOnlyFunction({
+      contractAddress,
+      contractName: "counter",
+      functionName: "get-user-count",
+      functionArgs: [Cl.standardPrincipal(userAddress)],
+      network: "devnet",
+      senderAddress: userAddress
+    })
+
+    setUserCount(Number(cvToValue(result)))
+  }
+
+  async function getUserCountTokenBalance() {
+    let result = await fetchCallReadOnlyFunction({
+      contractAddress,
+      contractName: "count-token",
+      functionName: "get-balance",
+      functionArgs: [Cl.standardPrincipal(userAddress)],
+      network: "devnet",
+      senderAddress: userAddress
+    })
+
+    setCountBalance(parseFloat(cvToValue(result).value!))
+  }
+
+  const client = createClient({
+    baseUrl: "http://localhost:3999"
+  })
+
+  async function getStxBalance() {
+    const { data } = await client.GET("/extended/v1/address/{principal}/stx", {
+      params: {
+        path: { principal: userAddress }
+      }
+    })
+
+    setStxBalance(Number(data?.balance!) / 1000000)
+  }
 
   function connectWallet() {
     showConnect({
@@ -34,21 +140,28 @@ export default function Home() {
   useEffect(() => {
     if (userSession.isUserSignedIn()) {
       setConnected(true)
+      setInterval(() => {
+        getUserCount()
+        getUserCountTokenBalance()
+        getStxBalance()
+      }, 3000)
     } else {
       setConnected(false)
     }
-  }, [])
+
+    setInterval(() => {
+      getTotalCount()
+    }, 3000)
+  }, [connected])
 
   return (
     <div className="relative grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       {connected ? (
         <div className="absolute top-10 right-10 flex gap-4 items-center flex-col sm:flex-row">
-          <span>1 STX | 1 COUNT</span>
           <span>
-            {userSession.loadUserData().profile.stxAddress.testnet.slice(0, 7) +
-              "..." +
-              userSession.loadUserData().profile.stxAddress.testnet.slice(-7)}
+            {stxBalance} STX | {countBalance} COUNT
           </span>
+          <span>{userAddress.slice(0, 7) + "..." + userAddress.slice(-7)}</span>
 
           <button
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -83,10 +196,16 @@ export default function Home() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           {connected ? (
             <>
-              <button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
+              <button
+                onClick={callDecrement}
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+              >
                 Decrement
               </button>
-              <button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
+              <button
+                onClick={callIncrement}
+                className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+              >
                 Increment
               </button>
             </>
